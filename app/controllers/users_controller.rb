21dd0b0b1a867c5826d5e5a0daf980a3d1de5fd7
Shamil <shamil@illustriam.com>
2014-06-12 00:36:06 -0400

    9.3 Showing All Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
@@ -8,6 +10,24 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def edit
+    # @user = User.find(params[:id])
+  end
+
+  def update
+    # @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+  
   def create
   	@user = User.new(user_params) #not the final implementation
   	if @user.save
@@ -24,6 +44,19 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password,
   								:password_confirmation)
-
   	end
+
+    #Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+  end
 end
